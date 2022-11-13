b0d888db8dc254df20b78d0f508380405fb9e6cb
Wasim Reja <iwasimreja@gmail.com>
2022-11-13 18:44:51 +0530

    New components for icons and page template

---
+++ src/components/Common/Footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <footer>
+            <p>
+                Created by <a className="rising-background" href="https://github.com/wasimreja" target="_blank" rel="noreferrer noopener">Wasim Reja</a>
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
