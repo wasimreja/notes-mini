b394ee2a4e60ff1c6731b38fb00659b3637478f1
Wasim Reja <iwasimreja@gmail.com>
2022-12-24 11:21:01 +0530

    Minor updates

--- src/components/Common/Footer.tsx
+++ src/components/Common/Footer.tsx
@@ -1,11 +1,11 @@
 function Footer() {
-    return (
-        <footer>
-            <p>
-                Created by <a className="rising-background" href="https://github.com/wasimreja" target="_blank" rel="noreferrer noopener">Wasim Reja</a>
-            </p>
-        </footer>
-    );
+	return (
+		<footer>
+			<p>
+				Created by <a className="rising-background" href="https://github.com/wasimreja" target="_blank" rel="noreferrer noopener">Wasim Reja</a>
+			</p>
+		</footer>
+	);
 }
 
 export default Footer;
